5a537764776deae0d0bd14599eeb89e6eb0ccfb7
Jan Poláček <gg.polacek@gmail.com>
2021-10-19 11:25:22 +0200

    Add files via upload

--- WAP/client/res/js/main.js
+++ WAP/client/res/js/main.js
@@ -3,7 +3,12 @@ const info = document.getElementById("info");
 window.onload = async () => {
     try {
         const res = await fetch('http://127.0.0.1:3000/user', {
-            method: "GET"
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ payload: {name: "Radek", age: 15} })
         });
         const data = await res.json();
         info.innerText = data.msg;
