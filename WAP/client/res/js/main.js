1c9e49330196ed7703760125d651942b7d29751a
Jan Poláček <gg.polacek@gmail.com>
2021-10-19 10:42:17 +0200

    Add files via upload

---
+++ WAP/client/res/js/main.js
@@ -0,0 +1,13 @@
+const info = document.getElementById("info");
+
+window.onload = async () => {
+    try {
+        const res = await fetch('http://127.0.0.1:3000/user', {
+            method: "GET"
+        });
+        const data = await res.json();
+        info.innerText = data.msg;
+    } catch (error) {
+        info.innerText = error;
+    }
+};
