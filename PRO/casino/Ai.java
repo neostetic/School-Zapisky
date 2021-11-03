cd46615e8487d865a5d752bf59b866be3de7219a
Jan Poláček <gg.polacek@gmail.com>
2021-11-03 13:52:28 +0100

    Add files via upload

--- PRO/casino/Ai.java
+++ PRO/casino/Ai.java
@@ -3,6 +3,19 @@ package cz.polacek.casino;
 public class Ai {
 
     public static void main(String[] args) {
+
+        aiBid();
+
     }
+
+    public static void aiBid() {
+        System.out.println("- AI's balance: " + Main.inventory);
+        System.out.print("AI's bid: ");
+        Main.bidValue = Main.inventory/10;
+        Main.bidColor = 2;
+        Main.bid();
+    }
+
+
 
 }
