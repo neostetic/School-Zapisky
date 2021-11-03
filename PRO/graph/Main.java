c9713434646bc17926e178d3b13793a207129cf5
Jan Poláček <gg.polacek@gmail.com>
2021-11-03 11:20:40 +0100

    Add files via upload

--- PRO/graph/Main.java
+++ PRO/graph/Main.java
@@ -1,6 +1,8 @@
 package cz.polacek.graph;
 
 import cz.polacek.graph.model.Point;
+import cz.polacek.graph.model.Point2D;
+import cz.polacek.graph.model.Point3D;
 import cz.polacek.graph.model.Vector;
 
 public class Main {
@@ -8,11 +10,13 @@ public class Main {
     public static void main(String[] args) {
 
         Point pointA = new Point(1,1,5);
-        Point pointB = new Point(4,8,7);
+        Point2D pointB = new Point2D(4,8);
+        Point3D pointC = new Point3D(4,8,4);
 
         Vector vector = pointA.getVector(pointB);
         System.out.println(pointA.toString());
         System.out.println(pointB.toString());
+        System.out.println(pointC.toString());
         System.out.println(vector.toString());
 
     }
---
+++ PRO/graph/model/Point3D.java
@@ -0,0 +1,9 @@
+package cz.polacek.graph.model;
+
+public class Point3D extends Point {
+
+    public Point3D(int x, int y, int z) {
+        super(x,y,z);
+    }
+
+}
