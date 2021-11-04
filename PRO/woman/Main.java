fcb423abb203770097f094146af741efc06c72bb
Jan Poláček <gg.polacek@gmail.com>
2021-11-04 14:56:48 +0100

    Add files via upload

---
+++ PRO/woman/Main.java
@@ -0,0 +1,10 @@
+package cz.polacek.uvodOOP;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Woman woman = new Woman("Marek", "@kvetak", 90, "blue");
+        woman.seznamitSeZenou();
+    }
+
+}
---
+++ PRO/woman/Woman.java
@@ -0,0 +1,45 @@
+package cz.polacek.uvodOOP;
+
+public class Woman {
+
+    String name;
+    String ig;
+    int age;
+    String eyeColor;
+
+    public Woman(String name, String ig, int age, String eyeColor) {
+        this.name = name;
+        this.ig = ig;
+        this.age = age;
+        this.eyeColor = eyeColor;
+    }
+
+    public Woman() {}
+
+    void cook() {
+        System.out.println(name + " is cooking...");
+    }
+
+    String cook(String food) {
+        return "Cooking" + food;
+    }
+
+    void seznamitSeZenou() {
+        System.out.println(
+                "Ahoj já jsem " + this.name + ", a můj Instagram, pod společností Facebook Incorporated\n" +
+                "(později Meta Incorporated) je " + this.ig + ". Ale jestli nechceš otravovat můj " + this.age + "\n" +
+                "ti letý účet, tak tě naučím anglicky barvu mých očí. To je: " + this.eyeColor + ", zbytek si\n" +
+                "vyřeš sám. Úchyle!11!1!!11"
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "Woman{" +
+                "name='" + name + '\'' +
+                ", ig='" + ig + '\'' +
+                ", age=" + age +
+                ", eyeColor='" + eyeColor + '\'' +
+                '}';
+    }
+}
